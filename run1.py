3ec572143e288f339cc02a4abb175f44681c031b
ming616 <ming616@gmail.com>
2020-08-07 13:50:30 +0800

    update

--- run1.py
+++ run1.py
@@ -6,7 +6,7 @@ from module import EE
 from config import config
 
 def one_test():
-    config.experiment_name = 'TransformerEncoderModel_clean_bio_unbert'  # 实验名称
+    config.experiment_name = 'TransformerEncoderModel_bio'  # 实验名称
     config.model_name = 'TransformerEncoderModel'  # 模型名称
     config.is_vector = False  # 是否使用bert词向量
     config.is_hidden_tag = False  # 是否增加 子句hidden-> 是否有待抽取属性 的约束
@@ -42,7 +42,7 @@ def many_test():
         config.embedding_size = test_dict['embedding_size'][i]
 
         ee = EE(config)
-        # ee.train()
+        ee.train()
         ee.predict_test()
         # ee.predict_sentence()
         # ee.test_format_result()
